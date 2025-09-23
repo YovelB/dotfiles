2da1ee50ecf168e55d54eaba730cd566e6188bb1
yovelb <yovelb2j@gmail.com>
2025-09-24 01:22:45 +0300

    feat(nvim): treej for better coding split (optional), rearenged some code

---
+++ .config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,18 @@
+return {
+  -- fun coding stats and achievements
+  {
+    "grzegorzszczepanek/gamify.nvim",
+    lazy = false,
+    config = function()
+      require("gamify")
+    end,
+  },
+  -- for splitting and joining code blocks
+  {
+    "Wansmer/treesj",
+    keys = {
+      { "<C-j>", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+    },
+    opts = { use_default_keymaps = false, max_join_length = 150 },
+  },
+}
