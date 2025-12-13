c0038a7bae5c0f9ebe26ceef1e6b5533dbe80fa1
yovelb <yovelb2j@gmail.com>
2025-12-13 10:45:59 +0200

    feat(terminal): dank material shell theme for all terminals + wezterm support for niri

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,17 +3,21 @@ local config = wezterm.config_builder()
 
 -- general
 config.term = "wezterm"
+config.enable_wayland = false
+config.webgpu_power_preference = "HighPerformance"
 config.scrollback_lines = 10000
+config.enable_kitty_graphics = true
 
 -- tab
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 -- window
 config.window_decorations = "NONE"
-config.window_padding = { left = 5, right = 0, top = 5, bottom = 0 }
+-- for gnome set left=5 and top=5 (for niri only top=4)
+config.window_padding = { left = 0, right = 0, top = 4, bottom = 0 }
 
 -- fonts
-config.font_size = 14.0
+config.font_size = 14.5
 config.font = wezterm.font_with_fallback({ "JetBrains Mono Nerd Font", "JetBrains Mono", "Noto Color Emoji" })
 -- colorscheme
 config.color_scheme = "Tokyo Night"
@@ -24,12 +28,12 @@ config.default_cursor_style = "SteadyBar"
 config.bidi_enabled = true
 config.bidi_direction = "LeftToRight"
 
--- fullscreen
+-- fullscreen (not needed for niri)
 -- this event triggers when WezTerm starts
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-	-- f11 fullscreen (for with os bars use maximize() instead)
-	window:gui_window():toggle_fullscreen()
-end)
+-- wezterm.on("gui-startup", function(cmd)
+-- 	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+-- 	-- f11 fullscreen (for with os bars use maximize() instead)
+-- 	window:gui_window():toggle_fullscreen()
+-- end)
 
 return config
